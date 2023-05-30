78212ef908da14dd9e5f46bc4599336cc0cd47c9
Martín Morondo <martinmorondo@gmail.com>
2023-05-29 22:14:51 -0300

    add background, font and font size customization options

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,133 @@
 import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
+import { useState } from 'react';
 
 const users = [
   {
     userName: 'martinMorondo',
     name: 'Martín Morondo',
     isFollowing: true,
+    followerCount: 1000,
   },
   {
     userName: 'lionelMessi',
     name: 'Lionel Messi',
     isFollowing: false,
+    followerCount: 100000,
   },
   {
     userName: 'Rihanna',
     name: 'Rihanna',
     isFollowing: true,
+    followerCount: 40000,
   },
   {
     userName: 'Shakira',
     name: 'Shakira',
     isFollowing: false,
+    followerCount: 50000,
   }
 ];
 
 export default function App() {
+  const [customizationOptions, setCustomizationOptions] = useState({
+    backgroundColor: 'black', // Valor inicial del color de fondo
+    fontFamily: 'Arial, sans-serif', // Valor inicial de la tipografía
+    fontSize: '14px', // Nuevo estado para el tamaño de fuente
+    textColor: '#fff', // Nuevo estado para el color del texto
+  });
+
+  const handleCustomizationChange = (options) => {
+    setCustomizationOptions(options);
+  };  
+
+  const handleColorChange = (color) => {
+    const updatedOptions = {
+      ...customizationOptions,
+      backgroundColor: color,
+    };
+    handleCustomizationChange(updatedOptions);
+  };
+  
+  const handleFontChange = (font) => {
+    const updatedOptions = {
+      ...customizationOptions,
+      fontFamily: font,
+    };
+    handleCustomizationChange(updatedOptions);
+  };
+
+  const handleFontSizeChange = (size) => {
+    const updatedOptions = {
+      ...customizationOptions,
+      fontSize: size,
+    };
+    handleCustomizationChange(updatedOptions);
+  };
+
+  const handleTextColorChange = (color) => {
+    const updatedOptions = {
+      ...customizationOptions,
+      textColor: color,
+    };
+    handleCustomizationChange(updatedOptions);
+  };
 
   return (
     <section className='App'>
       <h1>Twitter Follow Cards</h1>
-      {
-        users.map(({ userName, name, isFollowing}) => (
-          <TwitterFollowCard
-          key = {userName}
-          userName = {userName}
-          initialIsFollowing={isFollowing}
-          >
-            {name}
-          </TwitterFollowCard>
-        ))
-      }
-    </section>
-  )
-}
+      <div className="customization-controls">
+        <label className="background-label" htmlFor="color-picker">
+          Background Color:
+        </label>
+        <input
+          type="color"
+          id="color-picker"
+          value={customizationOptions.backgroundColor}
+          onChange={(e) => handleColorChange(e.target.value)}
+        />
+        <label className="text-label" htmlFor="text-color-picker">
+          Text Color:
+        </label>
+        <input
+          type="color"
+          id="text-color-picker"
+          value={customizationOptions.textColor}
+          onChange={(e) => handleTextColorChange(e.target.value)}
+        />
+        <label htmlFor="font-picker">Font Family:</label>
+        <select
+          id="font-picker"
+          value={customizationOptions.fontFamily}
+          onChange={(e) => handleFontChange(e.target.value)}
+        >
+          <option value="Arial, sans-serif">Arial</option>
+          <option value="Verdana, sans-serif">Verdana</option>
+          <option value="Times New Roman, serif">Times New Roman</option>
+        </select>
+        <label htmlFor="font-size-slider">Font Size:</label>
+        <input
+          type="range"
+          id="font-size-slider"
+          min="10"
+          max="24"
+          step="2"
+          value={parseInt(customizationOptions.fontSize)}
+          onChange={(e) => handleFontSizeChange(e.target.value + 'px')}
+        />
+      </div>
 
+      {users.map(({ userName, name, isFollowing, followerCount }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+          followerCount={followerCount}
+          customizationOptions={customizationOptions}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  );
+}
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
 
-export function TwitterFollowCard ({children, userName, initialIsFollowing }) {
-    const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+export function TwitterFollowCard({ children, userName, initialIsFollowing, followerCount, customizationOptions }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+  const [count, setCount] = useState(followerCount);
+  const [isFollowingAnimation, setIsFollowingAnimation] = useState(false);
 
-    const text = isFollowing ? 'Following' : 'Follow';
-    const buttonClassName = isFollowing
+  const text = isFollowing ? 'Following' : 'Follow';
+  const buttonClassName = isFollowing
     ? 'tw-followCard-button is-following'
     : 'tw-followCard-button';
 
     const handleClick = () => {
         setIsFollowing(!isFollowing);
-    }
+        setCount(isFollowing ? count - 1 : count + 1);
+        setIsFollowingAnimation(true);
+      
+        setTimeout(() => {
+          setIsFollowingAnimation(false);
+        }, 500); // Duración de la animación (0.5 segundos)
+      };
 
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar'
-                src = {`https://unavatar.io/${userName}`}
-                />
-                <div className='tw-followCard-info'>
-                    <strong>{children}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
-                </div>
-            </header>
+      const cardStyles = {
+        backgroundColor: customizationOptions.backgroundColor,
+        fontFamily: customizationOptions.fontFamily,
+        color: customizationOptions.textColor,
+      };
+      
+  return (
+    <article className='tw-followCard' style={cardStyles}>
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`} alt='Avatar' />
+        <div className='tw-followCard-info' style={{ fontSize: customizationOptions.fontSize, color: customizationOptions.textColor }}>
+          <strong>{children}</strong>
+          <span className='tw-followCard-infoUserName'>@{userName}</span>
+          <span className='tw-followCard-followerCount'>{count} followers</span>
+        </div>
+      </header>
 
-            <aside>
-                <button className={buttonClassName} onClick={handleClick}>
-                    <span className='tw-followCard-text'>{text}</span>
-                    <span className='tw-followCard-stopFollow'>Stop following</span>
-                </button>
-            </aside>
-        </article>
-    )
+      <aside>
+        <button className={`${buttonClassName} ${isFollowingAnimation ? 'is-animating' : ''}`} onClick={handleClick}>
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Stop following</span>
+        </button>
+      </aside>
+    </article>
+  );
 }
-
